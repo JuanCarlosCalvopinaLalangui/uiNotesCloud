a3bb4db366bc84d3e7729105cea9ac02b329c02d
Juan Carlos <2172742@my.ipleira.pt>
2024-04-09 00:48:41 +0100

    Feat:  API Config

--- dockerfile
+++ dockerfile
@@ -9,11 +9,12 @@ COPY package.json .
 COPY tsconfig.json .
 
 # Install the app dependencies for react typescript
-RUN npm install
+RUN npm install && npm cache clean --force
 
 # Copy the rest of the app files into the working directory
-COPY src/. ./src/
-COPY public/index.html ./public/index.html 
+#COPY src/. ./src/
+#COPY public/index.html ./public/index.html 
+COPY . .
 
 # Make port 3000 available outside the container
 # EXPOSE 3000
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 import "./App.css";
 import FormNotes from "./Components/forms";
 import Notes from "./Components/notes";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export interface Note {
   id: number;
@@ -9,18 +15,83 @@ export interface Note {
   content: string;
 }
 
-const App = () => {
+export interface CalendarItem {
+  id: string;
+  startDate: string;
+  endDate: string;
+  type: string;
+  name: { text: string }[];
+}
 
-  const [notes, setNotes] = useState<Note[]>([{id: 1, title: "Note 1", content: "This is the first note" }]);
+const App = () => {
+  useEffect(() => {
+    const fetchHolidays = async () => {
+      try {
+        // Fetch the data from the API
+        const response = await fetch(
+          "https://openholidaysapi.org/PublicHolidays?countryIsoCode=PT&languageIsoCode=EN&validFrom=2024-01-01&validTo=2024-12-30"
+        );
+        const calendar = await response.json();
+        console.log(calendar);
+        setCalendar(calendar);
+      } catch (error) {
+        console.error("Error fetching notes:", error);
+      }
+    };
+    fetchHolidays();
+  }, []);
+
+  const [calendar, setCalendar] = useState<CalendarItem[] | null>(null);
+  const [notes, setNotes] = useState<Note[]>([
+    { id: 1, title: "Note 1", content: "This is the first note" },
+  ]);
   const [selectedNote, setSelectedNote] = useState<Note | null>(null);
 
   return (
-    <div className="app-container">
-      <FormNotes notes={notes} setNotes={setNotes} setSelectedNote={setSelectedNote} selectedNote={selectedNote}></FormNotes>
-      <div  className="note-list">
-        <h1>Your notes</h1>
-        <Notes notes={notes} setSelectedNote={setSelectedNote} setNotes={setNotes}></Notes>
+    <div>
+      <div className="app-container">
+        <FormNotes
+          notes={notes}
+          setNotes={setNotes}
+          setSelectedNote={setSelectedNote}
+          selectedNote={selectedNote}
+        ></FormNotes>
+
+        <div className="note-list">
+          <h1>Your notes</h1>
+          <Notes
+            notes={notes}
+            setSelectedNote={setSelectedNote}
+            setNotes={setNotes}
+          ></Notes>
+        </div>
       </div>
+      <Box marginTop={5}>
+        {calendar ? (
+          <List
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+            bgcolor: "background.paper",
+            position: "relative",
+            overflow: "auto",
+            maxHeight: 600,
+            "& ul": { padding: 0 },
+          }}
+        >
+          {calendar.map((item) => (
+              <ListItem key={item.id}>
+                <ListItemText
+                  primary={item.name[0].text + " (" + item.type + ")" }
+                  secondary={item.startDate}
+                />
+              </ListItem>
+            ))}
+        </List>
+        ) : (
+          <h1>Something went wrong</h1>
+        )}
+      </Box>
     </div>
   );
 };
